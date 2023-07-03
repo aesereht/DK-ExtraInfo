2c9acf27e3a42219194a1c86d56dfa5e9287415f
aesereht <100576755+aesereht@users.noreply.github.com>
2023-07-04 00:10:31 +0200

    Update mod_main.gd
    Create massive universal mod_main.gd

--- HelloWorld-ExtraInfo/mod_main.gd
+++ HelloWorld-ExtraInfo/mod_main.gd
@@ -1,16 +1,43 @@
 extends Node
 
-const MOD_DIR = "HelloWorld-ExtraInfo/"
 const MOD_LOG = "HelloWorld-ExtraInfo"
+const MOD_DIR = MOD_LOG + "/"
 
 var dir = ""
-var ext_dir = ""
+var ext_dir = "extensions/"
+var trans_dir = "" #"translation/"
+var languages = [] #["en"]
+var yaml_dir = "" #"yaml/"
+var mode = 1 # 0-default / 1-Allow prestig / 2-Allow prestig+posting
+
+################################
+### Edit only above fragment ###
+################################
 
 func _init(modLoader = ModLoader):
 	ModLoaderLog.info("Init", MOD_LOG)
 	dir = ModLoaderMod.get_unpacked_dir() + MOD_DIR
-	ext_dir = dir + "extensions/"
+	ext_dir = dir + ext_dir
+	
+	# Install all gd files in "extensions/" directory and subdirecotries
 	get_dir_contents(ext_dir)
+	
+	# Add translation if exists
+	if trans_dir != null:
+		trans_dir = dir + trans_dir
+		for lang in languages:
+			ModLoaderMod.add_translation(trans_dir + MOD_LOG.to_lower() + "text." + lang + ".translation")
+	else:
+		pass
+	
+	match mode:
+		1:
+			CONST.PRESTIGE_WHITELIST_MOD_IDS.append(MOD_LOG)
+			CheatDetection.confirmedCheaters.append(Steam.getSteamID())
+		2:
+			CONST.PRESTIGE_WHITELIST_MOD_IDS.append(MOD_LOG)
+		_:
+			pass
 
 func get_dir_contents(rootPath: String) -> Array:
 	var files = []
@@ -32,15 +59,15 @@ func _add_dir_contents(dir: Directory, files: Array, directories: Array):
 		var path = dir.get_current_dir() + "/" + file_name
 
 		if dir.current_is_dir():
-			print("Found directory: %s" % path)
+			#print("Found directory: %s" % path) #Uncomment for debbuging
 			var subDir = Directory.new()
 			subDir.open(path)
 			subDir.list_dir_begin(true, false)
 			directories.append(path)
 			_add_dir_contents(subDir, files, directories)
 		else:
-			if path.get_extension() == "gd"
-				print("Installing: %s" % path)
+			if path.get_extension() == "gd": # For potential bug prevention
+				#print("Installing: %s" % path) #Uncomment for debbuging
 				ModLoaderMod.install_script_extension("%s" % path)
 
 		file_name = dir.get_next()
@@ -49,4 +76,14 @@ func _add_dir_contents(dir: Directory, files: Array, directories: Array):
 	
 func _ready():
 	ModLoaderLog.info("Done", MOD_LOG)
+	add_to_group("mod_init")
+
+func modInit():
+	if yaml_dir != "":
+		var pathToModYaml : String = ModLoaderMod.get_unpacked_dir() + MOD_DIR + yaml_dir
+		Data.parseUpgradesYaml(pathToModYaml + "upgrades.yaml")
+	#GameWorld.unlockElement(DOME_UPGRADE_NAME)
+	#This signal can be used to test the mod
+	#StageManager.connect("level_ready", self, "testMod")
+	pass
 
