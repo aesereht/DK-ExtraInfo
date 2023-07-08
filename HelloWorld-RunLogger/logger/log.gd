d864c5b2181f7f1e5c5a6ddd67d2232aa4420908
aesereht <100576755+aesereht@users.noreply.github.com>
2023-07-08 21:42:40 +0200

    Added time

--- HelloWorld-RunLogger/logger/log.gd
+++ HelloWorld-RunLogger/logger/log.gd
@@ -15,7 +15,7 @@ static func _write_to_log_file(string_to_write:String)->void :
 		var error: = log_file.open(MOD_DIR_PATH+"run_"+GameWorld.runId+".log", File.WRITE)
 		if not error == OK:
 			assert (false, "Could not open log file, error code: %s" % error)
-		log_file.store_string("%s Created log" % _get_date_string())
+		log_file.store_string("%s Created log" % _get_date_time_string())
 		log_file.store_string("\n" + "Version: %s" % GameWorld.version)
 		log_file.store_string("\n" + "RunID: %s" % GameWorld.runId)
 		log_file.close()
@@ -29,13 +29,20 @@ static func _write_to_log_file(string_to_write:String)->void :
 	log_file.store_string("\n" + string_to_write)
 	log_file.close()
 
-static func _get_date_string()->String:
-	var date_time: = Time.get_datetime_dict_from_system()
-	return "%s-%02d-%02d" % [date_time.year, date_time.month, date_time.day]
-
 
 func dir_check(path) -> void:
 	var dir = Directory.new()
 	if not dir.dir_exists(path):
 		dir.make_dir(path)
 		
+
+static func _get_date_string()->String:
+	var date_time: = Time.get_datetime_dict_from_system()
+	return "%s-%02d-%02d" % [date_time.year, date_time.month, date_time.day]
+	
+static func _get_time_string()->String:
+	var date_time: = Time.get_datetime_dict_from_system()
+	return "%02d:%02d:%02d" % [date_time.hour, date_time.minute, date_time.second]
+
+static func _get_date_time_string()->String:
+	return "%s_%s" % [_get_date_string(), _get_time_string()]
