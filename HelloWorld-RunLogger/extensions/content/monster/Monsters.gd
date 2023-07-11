27e1182dc16178f40b3c8019fc28878b838ed778
aesereht <100576755+aesereht@users.noreply.github.com>
2023-07-11 09:41:05 +0200

    Adding new things

--- HelloWorld-RunLogger/extensions/content/monster/Monsters.gd
+++ HelloWorld-RunLogger/extensions/content/monster/Monsters.gd
@@ -5,14 +5,14 @@ var tempArray = []
 
 func spawnWave():
 	.spawnWave()
-	LoggerMod.log_run(String(getNiceList(spawnPlan)), "WavePlan")
+	LoggerMod.log_run(String(Data.of("monsters.cycle")), "Wave")
+	LoggerMod.log_run(String(GameWorld.runTime), "RunTime")
 	LoggerMod.log_run(String(GameWorld.boughtUpgrades), "Upgrades")
+	LoggerMod.log_run(String(Level.mode.getRunWeight()), "RunWeight")
+	LoggerMod.log_run(String(getNiceList(spawnPlan)), "WavePlan")
 	LoggerMod.log_run("%d" % Level.map.get_node("TileData").get_resource_cells_by_id(0).size(), "IronOre")
 	LoggerMod.log_run("%d" % Level.map.get_node("TileData").get_resource_cells_by_id(1).size(), "WaterOre")
 	LoggerMod.log_run("%d" % Level.map.get_node("TileData").get_resource_cells_by_id(2).size(), "CobaltOre")
-	LoggerMod.log_run(String(GameWorld.runTime), "RunTime")
-	LoggerMod.log_run(String(Data.of("monsters.cycle")), "Wave")
-	LoggerMod.log_run(String(Level.mode.getRunWeight()), "RunWeight")
 	LoggerMod.log_run(String(Data.of("inventory.totaliron")), "TotalInvIron")
 	LoggerMod.log_run(String(Data.of("inventory.totalwater")), "TotalInvWater")
 	LoggerMod.log_run(String(Data.of("inventory.totalsand")), "TotalInvSand")
@@ -20,13 +20,14 @@ func spawnWave():
 
 func finishWave():
 	.finishWave()
-	LoggerMod.log_run(String(GameWorld.boughtUpgrades), "Upgrades")
 	LoggerMod.log_run(String(GameWorld.runTime), "RunTime")
+	LoggerMod.log_run(String(GameWorld.boughtUpgrades), "Upgrades")
 	LoggerMod.log_run(String(Level.monsters.waveDamageCounter), "Damage Counter")
 	LoggerMod.log_run(String(GameWorld.runStats.get("tiles_destroyed", 0)), "Tiles Destroyed")
 	LoggerMod.log_run(str(Data.of("prestige.baseperwave")), "IronMuli")
 	LoggerMod.log_run(str(Data.of("prestige.multiplier")), "CobaltMuli")
 	
+
 
 # Thanks to Rafa
 func getNiceList(entries:Array) -> Array:
---
+++ HelloWorld-RunLogger/extensions/stages/level/LevelStage.gd
@@ -0,0 +1,9 @@
+extends "res://stages/level/LevelStage.gd"
+
+var LoggerMod = load("res://mods-unpacked/HelloWorld-RunLogger/logger/log.gd").new()
+
+func showEndingPanel(popup):
+	.showEndingPanel(popup)
+	
+	LoggerMod.log_run(String(1 if GameWorld.won else 0), "WinCond")
+	LoggerMod.log_run(String(Recorder.tileEvents), "TileHistory")
